e79f82365c4587e71988a9797daf90f26d63e2c2
Cesar Henrique <cesaarhvb@gmail.com>
2025-02-04 22:20:07 -0300

    Add functionality to check phone number validity on button click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const clearButton = document.getElementById("clear-btn");
 const userInput = document.getElementById("user-input");
 const result = document.getElementById("results-div");
+const checkButton = document.getElementById("check-btn");
 
 const resetResult = () => {
   result.textContent = "";
@@ -10,4 +11,10 @@ const resetResult = () => {
 clearButton.addEventListener("click", () => {
   userInput.value = "";
   resetResult();
+});
+
+checkButton.addEventListener("click", () => {
+  const phoneUsRegex = /(?:1\s*)?/;
+  const value = userInput.value;
+  console.log(phoneUsRegex.test(value));
 });
